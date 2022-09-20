7db9b32a8ede81d94c92ae20cd2ef75dd1891673
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-20 09:47:03 -0500

    change board#initialize to include @dimensions

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 # class to represent a chess board
-
 class Board
-  attr_reader :pieces
-  def initialize
+  attr_reader :pieces, :dimensions
+
+  def initialize(x: 8, y: 8)
     @pieces = []
+    @dimensions = [x, y]
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,6 +9,10 @@ describe 'Board' do
       it 'has no pieces' do
         expect(board.pieces).to eql([])
       end
+
+      it 'has dimensions of [8, 8]' do
+        expect(board.dimensions).to eql([8, 8])
+      end
     end
   end
 end

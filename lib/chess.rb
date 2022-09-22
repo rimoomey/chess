22818c482244e098774daf458a644b703e3ff91d
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-22 10:44:17 -0500

    delete conditional logic

--- lib/chess.rb
+++ lib/chess.rb
@@ -20,34 +20,6 @@ class Chess
     create_pawns
     create_rooks
     create_bishops
-    PieceFactory::TYPES.each do |key, type|
-      if type == Pawn
-        # pawns = []
-        # 8.times do |time|
-        #   pawns.push(PieceFactory.for(type: key, x: 1 + time, y: 2, board: board, color: 'w'))
-        #   pawns.push(PieceFactory.for(type: key, x: 1 + time, y: 7, board: board, color: 'b'))
-        # end
-        # pawns.each { |pawn| board.add_piece(piece: pawn) }
-
-      elsif type == Rook
-        # rooks = []
-        # rooks.push(PieceFactory.for(type: key, x: 1, y: 1, board: board, color: 'w'))
-        # rooks.push(PieceFactory.for(type: key, x: 8, y: 1, board: board, color: 'w'))
-        # rooks.push(PieceFactory.for(type: key, x: 1, y: 8, board: board, color: 'b'))
-        # rooks.push(PieceFactory.for(type: key, x: 8, y: 8, board: board, color: 'b'))
-
-        # rooks.each { |rook| board.add_piece(piece: rook) }
-
-      elsif type == Bishop
-        # bishops = []
-        # bishops.push(PieceFactory.for(type: key, x: 3, y: 1, board: board, color: 'w'))
-        # bishops.push(PieceFactory.for(type: key, x: 6, y: 1, board: board, color: 'w'))
-        # bishops.push(PieceFactory.for(type: key, x: 3, y: 8, board: board, color: 'b'))
-        # bishops.push(PieceFactory.for(type: key, x: 6, y: 8, board: board, color: 'b'))
-
-        # bishops.each { |bishop| board.add_piece(piece: bishop) }
-      end
-    end
   end
 
   private
@@ -59,7 +31,6 @@ class Chess
       pawns.push(PieceFactory.for(type: :pawn, x: 1 + time, y: 7, board: board, color: 'b'))
     end
     pawns.each { |pawn| board.add_piece(piece: pawn) }
-    pawns
   end
 
   def create_rooks

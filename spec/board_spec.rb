9e8a196733c832da03e8851fc6b96b7eed7243e5
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-27 13:14:53 -0500

    move Movement module into own describe block

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -13,6 +13,92 @@ describe 'Board' do
     end
   end
 
+  describe '#in_bounds?' do
+    context 'when an index is within board dimensions' do
+      it 'is true' do
+        expect(board.in_bounds?(x: 1, y: 2)).to be(true)
+      end
+    end
+
+    context 'when x is out of bounds' do
+      it 'is false' do
+        expect(board.in_bounds?(x: 9, y: 2)).to be(false)
+      end
+    end
+
+    context 'when y is out of bounds' do
+      it 'is false' do
+        expect(board.in_bounds?(x: 3, y: -9)).to be(false)
+      end
+    end
+
+    context 'when x and y are out of bounds' do
+      it 'is false' do
+        expect(board.in_bounds?(x: -2,y: 9)).to be(false)
+      end
+    end
+  end
+
+  describe '#occupied?' do
+    context 'when a piece occupies a given space' do
+      before do
+        board.add_piece(piece: Piece.new(x: 1, y: 1, color: 'b'))
+      end
+
+      it 'returns true' do
+        expect(board.occupied?(x: 1, y: 1)).to be(true)
+      end
+    end
+
+    context 'when a piece does not occupy a given space' do
+      it 'returns false' do
+        expect(board.occupied?(x: 5, y: 4)).to be(false)
+      end
+    end
+  end
+
+  describe '#move_piece' do
+    let(:piece1) { Piece.new(x: 1, y: 1, color: 'w') }
+    let(:piece2) { Piece.new(x: 1, y: 2, color: 'b') }
+
+    context "when there's already a piece there" do
+      before do
+        board.add_piece(piece: piece1)
+        board.add_piece(piece: piece2)
+      end
+      it 'does not move the piece' do
+        board.move_piece(piece: piece2, x: 1, y: 1)
+        expect(board.pieces[1].current_space).to eql([1, 2])
+      end
+    end
+
+    context "when a piece is in the way" do
+      before do
+        board.add_piece(piece: piece1)
+        board.add_piece(piece: piece2)
+      end
+      it 'does not move the piece' do
+        board.move_piece(piece: piece2, x: 1, y: 3)
+        expect(board.pieces[1].current_space).to eql([1, 2])
+      end
+    end
+
+    context "when there is not already a piece there" do
+      before do
+        board.add_piece(piece: piece1)
+        board.add_piece(piece: piece2)
+      end
+      it 'moves the piece' do
+        board.move_piece(piece: piece2, x: 2, y: 2)
+        expect(board.pieces[1].current_space).to eql([2, 2])
+      end
+    end
+  end
+end
+
+describe 'Movement' do
+  subject(:board) { Board.new }
+
   describe '#piece_blocking_movement?' do
     context 'when piece is not in the way' do
       it 'is false' do
@@ -92,88 +178,6 @@ describe 'Board' do
                                               y: 7,
                                               target_x: 6,
                                               target_y: 5)).to be(true)
-      end
-    end
-  end
-
-  describe '#in_bounds?' do
-    context 'when an index is within board dimensions' do
-      it 'is true' do
-        expect(board.in_bounds?(x: 1, y: 2)).to be(true)
-      end
-    end
-
-    context 'when x is out of bounds' do
-      it 'is false' do
-        expect(board.in_bounds?(x: 9, y: 2)).to be(false)
-      end
-    end
-
-    context 'when y is out of bounds' do
-      it 'is false' do
-        expect(board.in_bounds?(x: 3, y: -9)).to be(false)
-      end
-    end
-
-    context 'when x and y are out of bounds' do
-      it 'is false' do
-        expect(board.in_bounds?(x: -2,y: 9)).to be(false)
-      end
-    end
-  end
-
-  describe '#occupied?' do
-    context 'when a piece occupies a given space' do
-      before do
-        board.add_piece(piece: Piece.new(x: 1, y: 1, color: 'b'))
-      end
-
-      it 'returns true' do
-        expect(board.occupied?(x: 1, y: 1)).to be(true)
-      end
-    end
-
-    context 'when a piece does not occupy a given space' do
-      it 'returns false' do
-        expect(board.occupied?(x: 5, y: 4)).to be(false)
-      end
-    end
-  end
-
-  describe '#move_piece' do
-    let(:piece1) { Piece.new(x: 1, y: 1, color: 'w') }
-    let(:piece2) { Piece.new(x: 1, y: 2, color: 'b') }
-
-    context "when there's already a piece there" do
-      before do
-        board.add_piece(piece: piece1)
-        board.add_piece(piece: piece2)
-      end
-      it 'does not move the piece' do
-        board.move_piece(piece: piece2, x: 1, y: 1)
-        expect(board.pieces[1].current_space).to eql([1, 2])
-      end
-    end
-
-    context "when a piece is in the way" do
-      before do
-        board.add_piece(piece: piece1)
-        board.add_piece(piece: piece2)
-      end
-      it 'does not move the piece' do
-        board.move_piece(piece: piece2, x: 1, y: 3)
-        expect(board.pieces[1].current_space).to eql([1, 2])
-      end
-    end
-
-    context "when there is not already a piece there" do
-      before do
-        board.add_piece(piece: piece1)
-        board.add_piece(piece: piece2)
-      end
-      it 'moves the piece' do
-        board.move_piece(piece: piece2, x: 2, y: 2)
-        expect(board.pieces[1].current_space).to eql([2, 2])
       end
     end
   end

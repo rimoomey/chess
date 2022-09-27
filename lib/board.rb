63827deccfb70ef921ad167742860c48fea5c601
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-27 13:12:48 -0500

    move piece_blocking_movement to movement

--- lib/board.rb
+++ lib/board.rb
@@ -14,18 +14,6 @@ class Board
     @pieces = []
   end
 
-  def piece_blocking_movement?(x:, y:, target_x:, target_y:)
-    spaces_to_check = spaces_between({ x: x, y: y },
-                                     { x: target_x, y: target_y })
-
-    spaces_to_check.each do |space|
-      pieces.each do |piece|
-        return true if piece.current_space == space
-      end
-    end
-    false
-  end
-
   def in_bounds?(x:, y:)
     x.positive? && x <= DIMENSIONS[0] && y.positive? &&
       y <= DIMENSIONS[1]
@@ -48,65 +36,5 @@ class Board
                                   piece_blocking_movement?(x: piece.current_space[0],
                                                            y: piece.current_space[0],
                                                            target_x: x, target_y: y)
-  end
-
-  private
-
-  def spaces_between(space1, space2)
-    if space1[:x] == space2[:x] && space1[:y] == space2[:y]
-      [space1[:x], space1[:y]]
-    elsif space1[:x] == space2[:x]
-      vertical_spaces_between(space1, space2)
-    elsif space1[:y] == space2[:y]
-      horizontal_spaces_between(space1, space2)
-    else
-      diagonal_spaces_between(space1, space2)
-    end
-  end
-
-  def vertical_spaces_between(space1, space2)
-    spaces_between = []
-    ((space1[:y] + 1)...space2[:y]).each do |y_index|
-      spaces_between.push([space1[:x], y_index])
-    end
-    spaces_between
-  end
-
-  def horizontal_spaces_between(space1, space2)
-    spaces_between = []
-    ((space1[:x] + 1)...space2[:x]).each do |x_index|
-      spaces_between.push([x_index, space1[:y]])
-    end
-    spaces_between
-  end
-
-  def diagonal_spaces_between(space1, space2)
-    spaces_between = []
-    (-8..8).each do |shift|
-      if space1[:x] + shift == space2[:x] && space1[:y] + shift == space2[:y]
-        if shift.positive?
-          (1..shift).each do |delta|
-            spaces_between.push([space1[:x] + delta, space1[:y] + delta])
-          end
-        else
-          (shift..-1).each do |delta|
-            spaces_between.push([space1[:x] + delta, space1[:y] + delta])
-          end
-        end
-      end
-
-      if space1[:x] - shift == space2[:x] && space1[:y] + shift == space2[:y]
-        if shift.positive?
-          (1..shift).each do |delta|
-            spaces_between.push([space1[:x] - delta, space1[:y] + delta])
-          end
-        else
-          (shift..-1).each do |delta|
-            spaces_between.push([space1[:x] - delta, space1[:y] + delta])
-          end
-        end
-      end
-    end
-    spaces_between
   end
 end
--- lib/modules/movement.rb
+++ lib/modules/movement.rb
@@ -12,4 +12,87 @@ module Movement
     end
     possibles
   end
+
+  def piece_blocking_movement?(x:, y:, target_x:, target_y:)
+    spaces_to_check = spaces_between({ x: x, y: y },
+                                     { x: target_x, y: target_y })
+
+    spaces_to_check.each do |space|
+      pieces.each do |piece|
+        return true if piece.current_space == space
+      end
+    end
+    false
+  end
+
+  private
+
+  def spaces_between(space1, space2)
+    if space1[:x] == space2[:x] && space1[:y] == space2[:y]
+      [space1[:x], space1[:y]]
+    elsif space1[:x] == space2[:x]
+      vertical_spaces_between(space1, space2)
+    elsif space1[:y] == space2[:y]
+      horizontal_spaces_between(space1, space2)
+    else
+      diagonal_spaces_between(space1, space2)
+    end
+  end
+
+  def vertical_spaces_between(space1, space2)
+    spaces_between = []
+    ((space1[:y] + 1)...space2[:y]).each do |y_index|
+      spaces_between.push([space1[:x], y_index])
+    end
+    spaces_between
+  end
+
+  def horizontal_spaces_between(space1, space2)
+    spaces_between = []
+    ((space1[:x] + 1)...space2[:x]).each do |x_index|
+      spaces_between.push([x_index, space1[:y]])
+    end
+    spaces_between
+  end
+
+  def diagonal_spaces_between(space1, space2)
+    spaces_between = []
+    (-8..8).each do |shift|
+      spaces_between += downward_diag(shift, space1, space2)
+      spaces_between += upward_diag(shift, space1, space2)
+    end
+    spaces_between
+  end
+
+  def downward_diag(shift, space1, space2)
+    downward_diag = []
+    if space1[:x] + shift == space2[:x] && space1[:y] + shift == space2[:y]
+      if shift.positive?
+        (1..shift).each do |delta|
+          downward_diag.push([space1[:x] + delta, space1[:y] + delta])
+        end
+      else
+        (shift..-1).each do |delta|
+          downward_diag.push([space1[:x] + delta, space1[:y] + delta])
+        end
+      end
+    end
+    downward_diag
+  end
+
+  def upward_diag(shift ,space1, space2)
+    upward_diag = []
+    if space1[:x] - shift == space2[:x] && space1[:y] + shift == space2[:y]
+      if shift.positive?
+        (1..shift).each do |delta|
+          upward_diag.push([space1[:x] - delta, space1[:y] + delta])
+        end
+      else
+        (shift..-1).each do |delta|
+          upward_diag.push([space1[:x] - delta, space1[:y] + delta])
+        end
+      end
+    end
+    upward_diag
+  end
 end

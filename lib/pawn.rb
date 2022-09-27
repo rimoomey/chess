24b9b7469f55af452efdbd84ac7d47f2e299f52b
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-27 12:48:05 -0500

    remove pawn#moved and update pawn@moves instead

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -4,16 +4,14 @@ require_relative 'piece'
 
 # pawn representation--inherits from Piece
 class Pawn < Piece
-  attr_reader :moved
 
   def initialize(x:, y:, color:)
     super(x: x, y: y, color: color)
     @moves = [[0, 1], [0, 2], [-1, 1], [1, 1]]
-    @moved = false
   end
 
   def move(x:, y:)
     super(x: x, y: y)
-    @moved = true
+    @moves = [[0, 1], [-1, 1], [1, 1]]
   end
 end

38ca8d55f3dd79477646e332a55a767366594389
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-28 16:20:14 -0500

    refactor game_state

--- lib/modules/game_state.rb
+++ lib/modules/game_state.rb
@@ -14,28 +14,34 @@ module GameState
                    [0, 0, 0, 0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0, 0, 0, 0],
                    [0, 0, 0, 0, 0, 0, 0, 0]]
-    i = nil
 
-    board.pieces.each do |piece|
-      if piece.instance_of?(Pawn)
-        i = 1
-      elsif piece.instance_of?(Rook)
-        i = 2
-      elsif piece.instance_of?(Knight)
-        i = 3
-      elsif piece.instance_of?(Bishop)
-        i = 4
-      elsif piece.instance_of?(Queen)
-        i = 5
-      elsif piece.instance_of?(King)
-        i = 6
+    board.game_state.each_with_index do |row, i|
+      row.each_with_index do |piece, j|
+        if piece.instance_of?(Pawn)
+          board_state[i][j] = 1
+        elsif piece.instance_of?(Rook)
+          board_state[i][j] = 2
+        elsif piece.instance_of?(Knight)
+          board_state[i][j] = 3
+        elsif piece.instance_of?(Bishop)
+          board_state[i][j] = 4
+        elsif piece.instance_of?(Queen)
+          board_state[i][j] = 5
+        elsif piece.instance_of?(King)
+          board_state[i][j] = 6
+        end
+
+        next if piece.instance_of?(Integer)
+
+        board_state[i][j] = -board_state[i][j] if piece.color == 'b'
       end
 
-      i = -i if piece.color.downcase == 'b'
+      # i = -i if piece.color.downcase == 'b'
 
-      board_state[piece.current_space[0] - 1][piece.current_space[1] - 1] = i
-      i = 0
+      # board_state[piece.current_space[0] - 1][piece.current_space[1] - 1] = i
+      # i = 0
     end
+
     board_state
   end
 end
--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -3,12 +3,12 @@
 require_relative '../lib/modules/game_state'
 
 describe 'GameState' do
-  subject(:game) { Chess.new(player1: 'player', player2: 'other_player', board: game_board)}
+  subject(:game) { Chess.new(player1: 'player', player2: 'other_player')}
   let(:game_board) { Board.new }
 
   describe '#state' do
     context 'when the game board is empty' do
-      xit 'returns [[0,..., 0],...,[0,..., 0]]' do
+      it 'returns [[0,..., 0],...,[0,..., 0]]' do
         expect(GameState::state(board: game_board)).to eql([[0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,10 +22,10 @@ describe 'GameState' do
 
     context 'when the game board has a pawn at d6' do
       before do
-        game_board.add_piece(piece: Pawn.new(x: 3, y: 4, color: 'w'))
+        game_board.add_piece(piece: Pawn.new(x: 2, y: 3, color: 'w'), location: [2, 3])
       end
 
-      xit 'returns a 1 in the proper place' do
+      it 'returns a 1 in the proper place' do
         expect(GameState::state(board: game_board)).to eql([[0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 1, 0, 0, 0, 0],
@@ -39,11 +39,11 @@ describe 'GameState' do
 
     context 'when the game board has a pawn at d6 and a king at e8' do
       before do
-        game_board.add_piece(piece: Pawn.new(x: 3, y: 4, color: 'w'))
-        game_board.add_piece(piece: King.new(x: 1, y: 5, color: 'w'))
+        game_board.add_piece(piece: Pawn.new(x: 2, y: 3, color: 'w'), location: [2, 3])
+        game_board.add_piece(piece: King.new(x: 0, y: 4, color: 'w'), location: [0, 4])
       end
 
-      xit 'returns a 1 and a 6 in the proper places' do
+      it 'returns a 1 and a 6 in the proper places' do
         expect(GameState::state(board: game_board)).to eql([[0, 0, 0, 0, 6, 0, 0, 0],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 1, 0, 0, 0, 0],
@@ -60,8 +60,8 @@ describe 'GameState' do
         game.new_game
       end
 
-      xit 'returns all proper places' do
-        expect(GameState::state(board: game_board)).to eql([[2, 0, 4, 0, 0, 4, 0, 2],
+      it 'returns all proper places' do
+        expect(GameState::state(board: game.board)).to eql([[2, 0, 4, 0, 0, 4, 0, 2],
                                                             [1, 1, 1, 1, 1, 1, 1, 1],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],

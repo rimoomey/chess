1ee534fc4058f1226d422288893f8310243bef2b
Rimon Moomey <rimon.moomey@gmail.com>
2022-09-29 11:51:45 -0500

    fix new_game check

--- spec/game_state_spec.rb
+++ spec/game_state_spec.rb
@@ -61,14 +61,14 @@ describe 'GameState' do
       end
 
       it 'returns all proper places' do
-        expect(GameState::state(board: game.board)).to eql([[2, 0, 4, 0, 0, 4, 0, 2],
+        expect(GameState::state(board: game.board)).to eql([[2, 3, 4, 6, 5, 4, 3, 2],
                                                             [1, 1, 1, 1, 1, 1, 1, 1],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
                                                             [0, 0, 0, 0, 0, 0, 0, 0],
                                                             [-1, -1, -1, -1, -1, -1, -1, -1],
-                                                            [-2, 0, -4, 0, 0, -4, 0, -2]])
+                                                            [-2, -3, -4, -6, -5, -4, -3, -2]])
       end
     end
   end

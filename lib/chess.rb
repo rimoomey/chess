5d8a35bbf1e4622e78ecb0af34e0c07c9e0806b3
Rimon Moomey <rimon.moomey@gmail.com>
2022-12-13 15:18:34 -0600

    reorganize file system

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'piece'
 require_relative 'modules/game_state'
 require_relative 'board'
 require_relative 'piece_factory'
--- lib/piece_factory.rb
+++ lib/piece_factory.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-require_relative 'pawn'
-require_relative 'rook'
-require_relative 'knight'
-require_relative 'bishop'
-require_relative 'queen'
-require_relative 'king'
+require_relative './pieces/pawn'
+require_relative './pieces/rook'
+require_relative './pieces/knight'
+require_relative './pieces/bishop'
+require_relative './pieces/queen'
+require_relative './pieces/king'
+require_relative './pieces/piece'
 
 # piece factory
 class PieceFactory
